4348faff836ae33740068d6db2424a24c5a6e0f6
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-10-19 11:21:45 +0500

    Geometry HARD in Progres

--- AbstractGeometry/Program.cs
+++ AbstractGeometry/Program.cs
@@ -1,6 +1,11 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 using System.Runtime.InteropServices;
-using System;
 
 namespace AbstractGeometry
 {
